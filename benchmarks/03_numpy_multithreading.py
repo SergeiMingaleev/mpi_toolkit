6598241d48f5a06d2ae7fabb65bb47fa02cac1bb
SergeiMingaleev <sergei.mingaleev@gmail.com>
2025-10-15 13:46:55 +0300

    Create 03_numpy_multithreading.py
    Добавлен тест поддержки многопотоковости в `numpy` для операции матричного умножения `numpy.dot()`.

---
+++ benchmarks/03_numpy_multithreading.py
@@ -0,0 +1,51 @@
+#------------------------------------------------------------------
+# Проверка скорости работы библиотеки `numpy` на одном
+# компьютере. Практически все современные сборки `numpy`
+# уже используют параллелизацию выполнения тех или иных
+# операций `numpy` в многоядерных системах, и для правильной
+# параллелизации своего кода важно понимать вашу ситуацию.
+#
+# https://superfastpython.com/numpy-multithreaded-parallelism/
+#------------------------------------------------------------------
+
+import os
+
+# Установим ограничение на число потоков,
+# используемых для векторных операций внутри
+# библиотеки `numpy`.
+# 1) это нужно делать до импорта самой `numpy`.
+# 2) разные сборки `numpy` используют разные
+#    реализации библиотек Blas и Lapack, и
+#    распараллеливание в них управляется разными
+#    переменными окружения - меняем их все для
+#    надёжности.
+
+N_THREADS = '1'
+#N_THREADS = '8'
+
+os.environ['OMP_NUM_THREADS'] = N_THREADS
+os.environ['OPENBLAS_NUM_THREADS'] = N_THREADS
+os.environ['MKL_NUM_THREADS'] = N_THREADS
+os.environ['VECLIB_MAXIMUM_THREADS'] = N_THREADS
+os.environ['NUMEXPR_NUM_THREADS'] = N_THREADS
+
+import numpy as np
+from cpu_timer import BenchmarkTimer
+timer = BenchmarkTimer()
+
+# Размер матриц будет NxN:
+N = 5000
+
+# Сделаем две случайные матрицы с размером NxN:
+np.random.seed(17)
+data1 = np.random.rand(N, N)
+data2 = np.random.rand(N, N)
+
+# Проверим скорость перемножения матриц:
+
+timer.start('dot')
+result = data1.dot(data2)
+timer.stop('dot')
+
+timer.print_status()
+#------------------------------------------------------------------
