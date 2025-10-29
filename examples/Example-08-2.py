47bae46b936accefb7ce0969dbe1c91526d5c34b
SergeiMingaleev <sergei.mingaleev@gmail.com>
2025-10-29 13:19:19 +0300

    Update Example-08-2.py
    Минимальные изменения примера - только по стилю записи.

--- examples/Example-08-2.py
+++ examples/Example-08-2.py
@@ -8,15 +8,15 @@ rank = comm.Get_rank()
 comm_cart = comm.Create_cart(dims=[numprocs], periods=[False], reorder=True)
 rank_cart = comm_cart.Get_rank()
 
-def u_init(x) :
+def u_init(x):
     u_init = np.sin(3*np.pi*(x - 1/6))
     return u_init
 
-def u_left(t) :
+def u_left(t):
     u_left = -1.
     return u_left
 
-def u_right(t) :
+def u_right(t):
     u_right = 1.
     return u_right
 
@@ -36,20 +36,20 @@ tau = (T - t_0) / M
 x = np.linspace(a, b, N+1)
 t = np.linspace(t_0, T, M+1)
 
-if rank_cart == 0 :
+if rank_cart == 0:
     ave, res = divmod(N + 1, numprocs)
     rcounts = np.empty(numprocs, dtype=np.int32)
     displs = np.empty(numprocs, dtype=np.int32)
-    for k in range(0, numprocs) : 
-        if k < res :
+    for k in range(0, numprocs):
+        if k < res:
             rcounts[k] = ave + 1
-        else :
+        else:
             rcounts[k] = ave
-        if k == 0 :
+        if k == 0:
             displs[k] = 0
-        else :
+        else:
             displs[k] = displs[k-1] + rcounts[k-1]   
-else :
+else:
     rcounts = None; displs = None
 
 N_part = np.array(0, dtype=np.int32)
@@ -57,17 +57,17 @@ N_part = np.array(0, dtype=np.int32)
 comm_cart.Scatter([rcounts, 1, MPI.INT],
                   [N_part, 1, MPI.INT], root=0)
 
-if rank_cart == 0 :
+if rank_cart == 0:
     rcounts_from_0 = np.empty(numprocs, dtype=np.int32)
     displs_from_0 = np.empty(numprocs, dtype=np.int32)
     rcounts_from_0[0] = rcounts[0] + 1
     displs_from_0[0] = 0
-    for k in range(1, numprocs-1) :
+    for k in range(1, numprocs-1):
         rcounts_from_0[k] = rcounts[k] + 2
         displs_from_0[k] = displs[k] - 1
     rcounts_from_0[numprocs-1] = rcounts[numprocs-1] + 1  
     displs_from_0[numprocs-1] = displs[numprocs-1] - 1
-else :
+else:
     rcounts_from_0 = None; displs_from_0 = None
 
 N_part_aux = np.array(0, dtype=np.int32)
@@ -80,28 +80,28 @@ comm_cart.Scatter([displs_from_0, 1, MPI.INT],
 
 u_part_aux = np.empty((M + 1, N_part_aux), dtype=np.float64)
 
-for n in range(N_part_aux) :
+for n in range(N_part_aux):
     u_part_aux[0, n] = u_init(x[displ_aux + n])   
-if rank_cart == 0 :
-    for m in range(1, M + 1) :
+if rank_cart == 0:
+    for m in range(1, M + 1):
         u_part_aux[m, 0] = u_left(t[m])  
-if rank_cart == numprocs-1 :
-    for m in range(1, M + 1) :
+if rank_cart == numprocs-1:
+    for m in range(1, M + 1):
         u_part_aux[m, N_part_aux - 1] = u_right(t[m])
 
-for m in range(M) :
-    for n in range(1, N_part_aux - 1) :
+for m in range(M):
+    for n in range(1, N_part_aux - 1):
         u_part_aux[m + 1, n] = u_part_aux[m, n] + \
             eps*tau/h**2*(u_part_aux[m, n+1] - 2*u_part_aux[m, n] + u_part_aux[m, n-1]) + \
                 tau/(2*h)*u_part_aux[m, n]*(u_part_aux[m, n+1] - u_part_aux[m, n-1]) + \
                     tau*u_part_aux[m, n]**3
 
-    if rank_cart == 0 :
+    if rank_cart == 0:
         comm_cart.Sendrecv(sendbuf=[u_part_aux[m+1, N_part_aux-2], 1, MPI.DOUBLE], 
                            dest=1, sendtag=0, 
                            recvbuf=[u_part_aux[m+1, N_part_aux-1:], 1, MPI.DOUBLE], 
                            source=1, recvtag=MPI.ANY_TAG, status=None)
-    if rank_cart in range(1, numprocs-1) :
+    if rank_cart in range(1, numprocs-1):
         comm_cart.Sendrecv(sendbuf=[u_part_aux[m+1, 1], 1, MPI.DOUBLE], 
                            dest=rank_cart-1, sendtag=0, 
                            recvbuf=[u_part_aux[m+1, 0:], 1, MPI.DOUBLE], 
@@ -110,30 +110,30 @@ for m in range(M) :
                            dest=rank_cart+1, sendtag=0, 
                            recvbuf=[u_part_aux[m+1, N_part_aux-1:], 1, MPI.DOUBLE], 
                            source=rank_cart+1, recvtag=MPI.ANY_TAG, status=None)
-    if rank_cart == numprocs-1 :
+    if rank_cart == numprocs-1:
         comm_cart.Sendrecv(sendbuf=[u_part_aux[m+1, 1], 1, MPI.DOUBLE], 
                            dest=numprocs-2, sendtag=0, 
                            recvbuf=[u_part_aux[m+1, 0:], 1, MPI.DOUBLE], 
                            source=numprocs-2, recvtag=MPI.ANY_TAG, status=None)
 
-if rank_cart == 0 :
+if rank_cart == 0:
     u_T = np.empty(N+1, dtype=np.float64)
-else : 
+else:
     u_T = None
 
-if rank_cart == 0 :
+if rank_cart == 0:
     comm_cart.Gatherv([u_part_aux[M, 0:N_part_aux-1], N_part, MPI.DOUBLE], 
                       [u_T, rcounts, displs, MPI.DOUBLE], root=0)
-if rank_cart in range(1, numprocs-1) :
+if rank_cart in range(1, numprocs-1):
     comm_cart.Gatherv([u_part_aux[M, 1:N_part_aux-1], N_part, MPI.DOUBLE], 
                       [u_T, rcounts, displs, MPI.DOUBLE], root=0)
-if rank_cart == numprocs-1 :
+if rank_cart == numprocs-1:
     comm_cart.Gatherv([u_part_aux[M, 1:N_part_aux], N_part, MPI.DOUBLE], 
                       [u_T, rcounts, displs, MPI.DOUBLE], root=0)
 
 end_time = MPI.Wtime()
 
-if rank_cart == 0 :
+if rank_cart == 0:
     import matplotlib.pyplot as plt
 
     print('N={}, M={}'.format(N, M))
