89f2643e83d0159b2ebb88582802c44ef030dfff
SergeiMingaleev <sergei.mingaleev@gmail.com>
2025-10-30 15:45:54 +0300

    Update Example-08-0.py
    Для удобства, переставлено местами определение констант.

--- examples/Example-08-0.py
+++ examples/Example-08-0.py
@@ -15,13 +15,14 @@ def u_right(t):
 
 start_time = time.perf_counter()
 
-a = 0.0; b = 1.0
-t_0 = 0.0; T = 6.0
-
 eps = 10**(-1.5)
+a = 0.0; b = 1.0
+t_0 = 0.0
 
-N = 800
-M = 300_000
+T = 6.0
+N = 800;    M = 300_000
+# T = 0.02
+# N = 8_000;  M = 100_000
 
 h = (b - a) / N
 tau = (T - t_0) / M
@@ -38,6 +39,7 @@ u[1:, N] = u_right(t[1:])
 
 eps_tau_h2 = eps*tau/h**2
 tau_2h = tau/(2*h)
+
 for m in range(M):
     u[m+1, 1:-1] = u[m,1:-1] + eps_tau_h2*(u[m,2:] - 2*u[m,1:-1] + u[m,:-2]) + \
         tau_2h*u[m,1:-1]*(u[m,2:] - u[m,:-2]) + tau*u[m,1:-1]**3
