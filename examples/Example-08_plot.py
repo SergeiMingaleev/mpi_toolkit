acd490c10544629c6802ca2e3c517c006a345f66
SergeiMingaleev <sergei.mingaleev@gmail.com>
2025-10-31 12:42:36 +0300

    Update Example-08_plot.py
    Добавил progress bar, чтобы видеть прогресс записи (слишком уж она бывает медленная).

--- examples/Example-08_plot.py
+++ examples/Example-08_plot.py
@@ -38,20 +38,22 @@ from celluloid import Camera
 
 
 #------------------------------------------------------------------
-# Примем как аргумент запуска программы имя файла данных
-# и проверим его:
+# Примем как аргументы запуска программы имя файла данных и
+# (опционально) шаг по моментам времени, которые нужно включить
+# в анимацию:
 
 if len(sys.argv) not in [2, 3]:
     print("\n"
-          "Please run the program with the name of the data file\n"
-          "which should be converted to the animation added as an argument.\n"
-          "EXAMPLE: python Example-08_plot.py Example-08-1_Results.npz\n"
+          "Пример запуска программы:\n"
+          ">> python Example-08_plot.py Example-08-1_Results.npz\n"
+          "где 'Example-08-1_Results.npz' - имя файла данных, для\n"
+          "которых нужно создать анимацию.\n"
           "\n"
-          "Optionally, you can also add as the last argument an integer\n"
-          "indicating the frame step that should be used for creating\n"
-          "the animation (by default, the frame step is 1, that is every\n"
-          "saved time frame is included into the animation).\n"
-          "EXAMPLE: python Example-08_plot.py Example-08-1_Results.npz 30"
+          "По умолчанию, анимация будет создана для всех записанных\n"
+          "моментов времени. Но вы можете добавить ещё один аргумент\n"
+          "(целое число) для указания шага по моментам времени,\n"
+          "которые будут включены в анимацию. Пример запуска:\n"
+          ">> python Example-08_plot.py Example-08-1_Results.npz 30\n"
           "\n")
     exit()
 
@@ -60,18 +62,17 @@ filename = sys.argv[1]
 filename_base, filename_ext = os.path.splitext(filename)
 
 if filename_ext != ".npz":
-    raise IOError(f"Expected a file with the extension '.npz', but got: '{filename}'")
+    raise IOError(f"Ожидался файл с расширением '.npz', а получен: '{filename}'")
 
 if not os.path.isfile(filename):
-    raise FileNotFoundError(f"Cannot find the file '{filename}'")
+    raise FileNotFoundError(f"Не могу найти файл '{filename}'")
 
 frame_step = 1
 if len(sys.argv) == 3:
     frame_step = int(sys.argv[2])
 
-print(f"Converting data file '{filename}' to animation file '{filename_base}.mp4'\n"
-      f"using the time frame step {frame_step}.\n")
-print("Please wait...")
+print(f"Конвертируем файл данных '{filename}' в файл анимации '{filename_base}.mp4'\n"
+      f"используя шаг {frame_step} по записанным моментам времени.\n")
 
 #------------------------------------------------------------------
 # Зачитаем файл данных и извлечём из него массивы `x`, `t`, и `u`:
@@ -86,6 +87,30 @@ u = results['u']
 xmin = np.min(x)
 xmax = np.max(x)
 
+#------------------------------------------------------------------
+# Поскольку создание видео занимает время, добавим простой
+# progress bar, который будет использоваться внутри цикла
+# по `m` и внутри вызова `animation.save()`:
+progress_bar_prefix = '    Рисуем:'
+M = int(len(u) / frame_step)
+
+def progress_bar(current_frame: int, total_frames: int):
+    global progress_bar_prefix, M
+    # Есть проблема - в нашем случае matplotlib не заполняет
+    # правильно значение `total_frames` - see comment in:
+    # https://github.com/matplotlib/matplotlib/blob/4aeb773422464799998d900198b35cb80e94b3e1/lib/matplotlib/animation.py#L1111
+    if total_frames is None:
+        total_frames = M
+    prefix = progress_bar_prefix
+    suffix = 'Complete'; fill = '█'
+    bar_length = 60
+    percent = ("{0:.1f}").format(100 * current_frame / total_frames)
+    filled_length = int(round(bar_length * current_frame / total_frames))
+    bar = fill * filled_length + '-' * (bar_length - filled_length)
+    sys.stdout.write(f'\r{prefix} |{bar}| {percent}% {suffix}   ')
+    sys.stdout.flush()
+
+#------------------------------------------------------------------
 # Готовимся рисовать найденное решение для
 # нужных моментов времени:
 plt.style.use('dark_background')
@@ -103,14 +128,18 @@ for m in range(0, len(u), frame_step):
     ax.text(0.8, -1.6, f't = {t[m]:.3f}')
     # Добавляем его в анимацию:
     camera.snap()
+    progress_bar(m // frame_step, M)
 # Анимируем набранные кадры:
 animation = camera.animate(interval=15, repeat=False, blit=True)
-# И записываем их в файл в формате MPEG-4 (но можете поиграться
-# с другими форматами - используйте расширение файла '.avi' и т.п.):
-animation.save(filename_base + '.mp4')
 # NOTE: Объект `animation` является экземпляром класса
 # `matplotlib.animation.ArtistAnimation`, и он может записывать
 # анимации в разных форматах - читайте его документацию:
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.animation.ArtistAnimation.html
-print("Done.")
+
+# Наконец, запишем анимацию в файл в формате MPEG-4 (но можете поиграться
+# с другими форматами - используйте расширение файла '.avi' и т.п.).
+progress_bar_prefix = 'Записываем:'
+animation.save(filename_base + '.mp4',
+               progress_callback=progress_bar)
+
 #------------------------------------------------------------------
